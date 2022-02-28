b1c4a5a6e38f47c0922cf0a2645acb6131037adc
Callum1scott <61849378+Callum1scott@users.noreply.github.com>
2022-02-28 17:31:06 -0500

    Script upload

---
+++ TheBoogeyman/Assets/TopDownMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMove : MonoBehaviour
+{
+    //Movement/Position variables
+    public Rigidbody2D player;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //actually moves player
+        player.velocity = new Vector2(Input.GetAxis("Horizontal")*3, Input.GetAxis("Vertical")*3);
+        
+    }
+}
