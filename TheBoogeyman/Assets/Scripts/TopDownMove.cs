ab81635822f936082679721bc96eb1fc937bb69d
Callum1scott <61849378+Callum1scott@users.noreply.github.com>
2022-03-19 01:11:14 -0400

    Claire kinda fixed
    Kinda fixed claire and fit her to a new script

--- TheBoogeyman/Assets/Scripts/TopDownMove.cs
+++ TheBoogeyman/Assets/Scripts/TopDownMove.cs
@@ -8,6 +8,9 @@ public class TopDownMove : MonoBehaviour
     public Rigidbody2D player;
     public float speed;
     public bool interact = false;
+    public SpriteRenderer ply;
+
+    public Animator PlayAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,51 @@ public class TopDownMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ply.flipX = false;
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            PlayAnim.SetFloat("Down",1);
+
+            PlayAnim.SetFloat("Up",0);
+            PlayAnim.SetFloat("Side",0);
+            PlayAnim.SetFloat("Idle",0);
+
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            PlayAnim.SetFloat("Side", 1);
+
+            PlayAnim.SetFloat("Up", 0);
+            PlayAnim.SetFloat("Down", 0);
+            PlayAnim.SetFloat("Idle", 0);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            PlayAnim.SetFloat("Side", 1);
+            ply.flipX = true;
+
+            PlayAnim.SetFloat("Up", 0);
+            PlayAnim.SetFloat("Down", 0);
+            PlayAnim.SetFloat("Idle", 0);
+        }
+        else if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            PlayAnim.SetFloat("Up",1);
+
+            PlayAnim.SetFloat("Down", 0);
+            PlayAnim.SetFloat("Side", 0);
+            PlayAnim.SetFloat("Idle", 0);
+        }
+        else
+        {
+            PlayAnim.SetFloat("Idle",1);
+
+            PlayAnim.SetFloat("Up", 0);
+            PlayAnim.SetFloat("Side", 0);
+            PlayAnim.SetFloat("Down", 0);
+        }
+        
         //actually moves player
         player.velocity = new Vector2((Input.GetAxis("Horizontal")*speed), (Input.GetAxis("Vertical")*speed));
 
